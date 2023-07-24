5f77db4909b2ddab35e6b6ea7ff66ca07297eea1
weaponcrafts <weaponscrafter@gmail.com>
2023-07-24 10:55:02 +0000

    Commit release v9.4.4.

---
+++ _next/static/1uqqNA2XZxv2iZToVftcc/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fbulletins","\u002Fadmin\u002Flogin","\u002Fsuperadmin\u002Flogin","\u002Fagroclimatic-services","\u002Fcropping-calendar","\u002Fweather-services","\u002Fbulletins\u002Fseasonal-outlook","\u002Fbulletins\u002Fspecial-weather-forecast","\u002Fbulletins\u002Fweather"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
