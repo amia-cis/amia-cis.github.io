8cc1b6e035a74cb0f429d490035d5a46d84bd46d
weaponcrafts <weaponscrafter@gmail.com>
2024-07-08 16:45:46 +0800

    feat: commit build output

---
+++ _next/static/Cc9k6PABScEqCcojbuMID/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2f71e0d51b6954c9.css","static/chunks/pages/index-7edb3527a47004ae.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
